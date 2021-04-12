1e0b1f447139d420b82226bac848f25bec127e72
董鹏飞 <dongpf@bingosoft.net>
2021-04-12 14:00:25 +0800

    表格

--- iview-dpf/src/pages/components/bmsa-table/index.vue
+++ iview-dpf/src/pages/components/bmsa-table/index.vue
@@ -1,6 +1,47 @@
 <template>
     <div class="content-inner">
-        <bmsa-table></bmsa-table>
+        <!--<bmsa-table></bmsa-table>-->
+        <vxe-toolbar>
+            <template #buttons>
+                <Button type="primary" @click="allAlign = 'left'">
+                    <Icon type="md-trash" size="16" />居左
+                </Button>
+                <Button type="primary" @click="allAlign = 'center'">居中</Button>
+                <Button type="primary" @click="allAlign = 'right'">居右</Button>
+            </template>
+        </vxe-toolbar>
+    
+        <vxe-table ref="xTable1"
+                    :align="allAlign"
+                   :data="tableData"
+                   :cell-style="cellStyle"
+                   resizable
+                   show-header-overflow
+                   class="mytable-scrollbar"
+                   border
+                   height="400px"
+                   highlight-hover-row
+                   @radio-change="radioChangeEvent">
+            <vxe-table-column type="seq" title="序号" width="60" align="center" fixed="left"></vxe-table-column>
+            <vxe-table-column type="radio" width="60" align="center" >
+                <template #header>
+                    <vxe-button type="text" @click="clearRadioRowEevnt" :disabled="!selectRow">取消</vxe-button>
+                </template>
+            </vxe-table-column>
+            <vxe-table-column field="name" title="Name" fixed="left" :formatter="formatterNum" width="120"></vxe-table-column>
+            <vxe-table-column field="sex" title="Sex" :filters="[{label: 'Man', value: 'Man'}, {label: 'Woman', value: 'Woman'}]" width="200">
+            </vxe-table-column>
+            <vxe-table-column type="html" field="address" show-overflow title='<span style="color: blue">HTML 标签与格式化</span>' width="200"></vxe-table-column>
+            <vxe-table-column field="age" title="Age" sortable :filters="[{ data: '' }]" :filter-method="filterAgeMethod" width="200">
+                <template #filter="{ $panel, column }">
+                    <input type="type" v-for="(option, index) in column.filters" :key="index" v-model="option.data" @input="$panel.changeOption($event, !!option.data, option)">
+                </template>
+            </vxe-table-column>
+            <vxe-table-column field="face" title="政治面貌" width="200"></vxe-table-column>
+            <vxe-table-column field="email" title="邮箱" width="200"></vxe-table-column>
+            <vxe-table-column field="number" title="身份证号" width="200"></vxe-table-column>
+            <vxe-table-column field="school" title="学校" width="200"></vxe-table-column>
+        </vxe-table>
     </div>
 </template>
 <script>
@@ -12,17 +53,93 @@
         },
         data () {
             return {
+                allAlign: null,
+                tableData: [
+                    { id: 10001, name: 'Test1', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'Develop', sex: 'Man', age: 28, address: 'vxe-table 从入门到放弃，超长字符串，超长字符串，超长字符串，超长字符串' },
+                    { id: 10002, name: 'Test2', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'Test', sex: 'Women', age: 22, address: '<img height="40" src="https://xuliangzhan_admin.gitee.io/vxe-table/static/other/img1.gif">' },
+                    { id: 10003, name: 'Test3', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+                    { id: 10004, name: 'Test4', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'Designer', sex: 'Women ', age: 24, address: 'Shanghai' },
+                    { id: 10005, name: 'Test3', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+                    { id: 10006, name: 'Test4', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'Designer', sex: 'Women ', age: 24, address: 'Shanghai' },
+                    { id: 10005, name: 'Test3', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+                    { id: 10006, name: 'Test4', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'Designer', sex: 'Women ', age: 24, address: 'Shanghai' },
+                    { id: 10005, name: 'Test3', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'PM', sex: 'Man', age: 32, address: 'Shanghai' },
+                    { id: 10006, name: 'Test4', number: '220182199304115336', school: '清华大学', face: '党员', email: '249009909@qq.com', role: 'Designer', sex: 'Women ', age: 24, address: 'Shanghai' }
+                ],
+                selectRow: null, // 单选
             }
         },
         computed: {},
         created () {},
         mounted () {},
-        methods: {}
+        methods: {
+            // 格式化姓名
+            formatterNum ({ cellValue, row, column }) {
+                return cellValue + '自定义'
+            },
+            // 自定义单元格样式
+            cellStyle ({ row, rowIndex, column }) {
+                if (column.property === 'sex') {
+                    if (row.sex === 'Women') {
+                        return {
+                            backgroundColor: '#187',
+                            color: '#ffffff'
+                        }
+                    } else if (row.age === 28) {
+                        return {
+                            backgroundColor: '#2db7f5',
+                            color: '#000'
+                        }
+                    }
+                }
+            },
+            // 单选
+            radioChangeEvent ({ row }) {
+                this.selectRow = row
+                console.log('单选事件')
+            },
+            // 单选
+            clearRadioRowEevnt () {
+                this.selectRow = null
+                this.$refs.xTable1.clearRadioRow()
+            },
+            // 筛选年龄
+            filterAgeMethod ({ option, row }) {
+                return row.age === Number(option.data)
+            }
+        }
     };
 </script>
 <style lang="less" scoped>
     .demo-table {
         width: 100%;
         height: 100%;
+    }
+
+    /*滚动条整体部分*/
+    .mytable-scrollbar ::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+    }
+    /*滚动条的轨道*/
+    .mytable-scrollbar ::-webkit-scrollbar-track {
+        background-color: #FFFFFF;
+    }
+    /*滚动条里面的小方块，能向上向下移动*/
+    .mytable-scrollbar ::-webkit-scrollbar-thumb {
+        background-color: #bfbfbf;
+        border-radius: 5px;
+        border: 1px solid #F1F1F1;
+        box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    }
+    .mytable-scrollbar ::-webkit-scrollbar-thumb:hover {
+        background-color: #A8A8A8;
+    }
+    .mytable-scrollbar ::-webkit-scrollbar-thumb:active {
+        background-color: #787878;
+    }
+    /*边角，即两个滚动条的交汇处*/
+    .mytable-scrollbar ::-webkit-scrollbar-corner {
+        background-color: #FFFFFF;
     }
 </style>
