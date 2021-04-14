306bad6d696f25d6d9710c9c61225a26ee801b6b
董鹏飞 <dongpf@bingosoft.net>
2021-04-14 16:11:43 +0800

    框架升级

--- iview-dpf/src/menu/sider.js
+++ iview-dpf/src/menu/sider.js
@@ -11,5 +11,5 @@ export default [
     dashboard,
     home,
     system,
-    bmsa,
+    bmsa
 ];
--- iview-dpf/src/router/routes.js
+++ iview-dpf/src/router/routes.js
@@ -64,7 +64,7 @@ const frameIn = [
     system,
     dashboard,
     home,
-    bmsa,
+    bmsa
 ];
 
 /**
