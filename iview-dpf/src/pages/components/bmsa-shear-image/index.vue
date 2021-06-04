6ac4feb49ecd9f1d93d9aaf48988e5301349f0e8
董鹏飞 <dongpf@bingosoft.net>
2021-06-04 09:06:07 +0800

    组件api

--- iview-dpf/src/pages/components/bmsa-shear-image/index.vue
+++ iview-dpf/src/pages/components/bmsa-shear-image/index.vue
@@ -1,9 +1,21 @@
 <template>
     <div class="content-inner">
-       <bmsa-shear-image :src="exampleImageSrc"
-                         crop-button-text="确认提交"
-                         @on-crop="handleCroped">
-       </bmsa-shear-image>
+        <Card>
+            <Split v-model="split1">
+                <div slot="left" class="demo-split-pane">
+                    <bmsa-shear-image :src="exampleImageSrc"
+                                      crop-button-text="确认提交"
+                                      @on-crop="handleCroped">
+                    </bmsa-shear-image>
+                </div>
+                <div slot="right" class="demo-split-pane">
+                    <Divider>详细描述</Divider>
+                    <h3>使用注意事项</h3>
+                    <p style="margin-top: 12px">1、必备src属性，代表上传地址</p>
+                    <p style="margin-top: 12px">2、on-crop回调代表上传成功</p>
+                </div>
+            </Split>
+        </Card>
     </div>
 </template>
 <script>
@@ -15,6 +27,7 @@
         },
         data () {
             return {
+                split1: 0.7,
                 exampleImageSrc: ''
             }
         },
