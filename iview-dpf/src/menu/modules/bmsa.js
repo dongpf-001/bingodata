b8dbaf2f29516a102c5c76e4bd91289770def59d
董鹏飞 <dongpf@bingosoft.net>
2021-06-04 09:16:57 +0800

    菜单分类

--- iview-dpf/src/menu/modules/bmsa.js
+++ iview-dpf/src/menu/modules/bmsa.js
@@ -7,20 +7,32 @@ export default {
     custom: 'i-icon-demo i-icon-demo-dashboard',
     children: [
         {
-            path: `${pre}bmsa-table`,
-            title: '表格'
+            path: '/form',
+            title: '表单组件',
+            children: [
+                {
+                    path: `${pre}bmsa-drop-grid`,
+                    title: '下拉表格'
+                },
+            ]
         },
         {
-            path: `${pre}bmsa-shear-image`,
-            title: '图片裁剪'
-        },
-        {
-            path: `${pre}bmsa-org-tree`,
-            title: '组织机构'
-        },
-        {
-            path: `${pre}bmsa-drop-grid`,
-            title: '下拉表格'
+            path: '/function',
+            title: '功能组件',
+            children: [
+                {
+                    path: `${pre}bmsa-table`,
+                    title: '表格'
+                },
+                {
+                    path: `${pre}bmsa-shear-image`,
+                    title: '图片裁剪'
+                },
+                {
+                    path: `${pre}bmsa-org-tree`,
+                    title: '组织机构'
+                },
+            ]
         },
     ]
 }
