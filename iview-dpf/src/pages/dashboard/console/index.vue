9896904c0cb58e66d3e8927346ce7bd7095a6f81
董鹏飞 <dongpf@bingosoft.net>
2021-06-07 14:08:16 +0800

    首页

--- iview-dpf/src/pages/dashboard/console/index.vue
+++ iview-dpf/src/pages/dashboard/console/index.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="console-wrapper">
+        <vue-mindmap
+                :nodes="nodes"
+                :connections="connections"
+                :editable="true">
+        </vue-mindmap>
     </div>
 </template>
 <script>
@@ -8,6 +13,27 @@
         components: {},
         data () {
             return {
+                nodes: [
+                    {
+                        'text': 'python',
+                        'url': 'http://www.wikiwand.com/en/Python_(programming_language)',
+                        'fx': -13.916222252976013,
+                        'fy': -659.1641376795345,
+                        'category': 'wiki',
+                        'note': ''
+                    }
+                ],
+                connections: [
+
+                    {
+                        'source': 'python',
+                        'target': 'basics',
+                        'curve': {
+                            'x': -43.5535,
+                            'y': 299.545
+                        }
+                    }
+                ]
             }
         },
         methods: {
